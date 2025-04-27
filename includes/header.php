d99c7bc5bcbb9f34d90b7a28b01d91532058d89a
qusar <qusar@ubun.myguest.virtualbox.org>
2025-04-27 20:30:13 +0300

    обновление стилей

--- includes/header.php
+++ includes/header.php
@@ -7,15 +7,23 @@ $menuItems = [
 ?>
 
 <header>
-    <nav>
-        <ul>
-            <?php foreach ($menuItems as $title => $url): ?>
-                <li>
-                    <a href="<?= $url ?>" <?= $_SERVER['REQUEST_URI'] == $url ? 'class="active"' : '' ?>>
-                        <?= $title ?>
-                    </a>
-                </li>
-            <?php endforeach; ?>
-        </ul>
+    <nav class="main-nav">
+        <div class="nav-container">
+            <div class="menu-toggle">
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+            </div>
+            
+            <ul class="nav-links">
+                <?php foreach ($menuItems as $title => $url): ?>
+                    <li>
+                        <a href="<?= $url ?>" class="<?= $_SERVER['REQUEST_URI'] == $url ? 'active' : '' ?>">
+                            <?= $title ?>
+                        </a>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
     </nav>
 </header>
